8376b711978edc72882f930a7a4d4caaef348107
Rerurate514 <rerurateyuto@gmail.com>
2026-05-19 18:47:50 +0900

    dbカラムを変更

--- app/infrastructure/db/db.ts
+++ app/infrastructure/db/db.ts
@@ -3,12 +3,13 @@ import { Database } from 'bun:sqlite'
 const db = new Database("curate.db")
 
 db.run(`
-    CREATE TABLE IF NOT EXIST users(
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
+    CREATE TABLE IF NOT EXISTS users (
+        id TEXT PRIMARY KEY,
         username TEXT UNIQUE NOT NULL,
         password_hash TEXT NOT NULL,
-        role TEXT NOT NULL CHECK(role IN ('admin', 'user', 'guest')) DEFAULT 'user'
-    )  
+        role TEXT NOT NULL CHECK(role IN ('admin', 'user', 'guest')),
+        created_at TEXT DEFAULT (datetime('now', 'utc'))
+    );
 `)
 
 export { db }
