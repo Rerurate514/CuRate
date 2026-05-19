57881f3762c5c4f6911bd6b920c1bfb8b8aa81cc
Rerurate514 <rerurateyuto@gmail.com>
2026-05-19 20:46:06 +0900

    初期化確認usecaseを追加

---
+++ app/usecases/check_initialize_usecase.ts
@@ -0,0 +1,12 @@
+import { Result } from "../core/utils/result";
+import { IDbUsersRepository } from "../domain/repositories/i_db_users_repository";
+
+export class CheckInitializeUsecase { 
+    constructor(
+        private readonly userRepo: IDbUsersRepository
+    ) { }
+
+    async execute(): Promise<Result<boolean>> {
+        return await this.userRepo.existsAnyUser();
+    }
+}
