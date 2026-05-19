e94a8087b2f12160e0711e44947670f7985f998c
Rerurate514 <rerurateyuto@gmail.com>
2026-05-19 18:36:32 +0900

    userrolesの追加

--- app/domain/entities/user.entity.ts
+++ app/domain/entities/user.entity.ts
@@ -1,7 +1,10 @@
+import { UserRoles } from "../vo/user_roles";
+
 export class UserEntity {
     private constructor(
         public readonly id: string,
         public readonly name: string,
-        public readonly passwordHash: string
+        public readonly passwordHash: string,
+        public readonly role: UserRoles
     ) {}
 }
