44d4bba7c156bc6a5809500a0434e408eda41ff6
Rerurate514 <rerurateyuto@gmail.com>
2026-05-19 17:33:04 +0900

    dockerとviteの設定を変更

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,27 @@
 import build from '@hono/vite-build/cloudflare-workers'
-import adapter from '@hono/vite-dev-server/cloudflare'
+import adapter from '@hono/vite-dev-server/node'
 import tailwindcss from '@tailwindcss/vite'
 import honox from 'honox/vite'
 import { defineConfig } from 'vite'
 
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
+  server: {
+    host: '0.0.0.0',
+    port: 5173,
+    watch: {
+      usePolling: true,
+      interval: 100
+    },
+    hmr: {
+      clientPort: 5173
+    }
+  },
   plugins: [
     honox({
       devServer: { adapter },
       client: { input: ['/app/client.ts', '/app/style.css'] }
     }),
     tailwindcss(),
-    build()
+    mode === 'production' && build()
   ]
-})
+}))
