efd14db03544bfe150c51cbc9b6ab3a64daf90dc
Rerurate514 <rerurateyuto@gmail.com>
2026-05-19 21:27:45 +0900

    余計なコードを削除

--- vite.config.ts
+++ vite.config.ts
@@ -1,27 +1,14 @@
-import build from '@hono/vite-build/cloudflare-workers'
-import adapter from '@hono/vite-dev-server/node'
+import adapter from '@hono/vite-dev-server/bun'
 import tailwindcss from '@tailwindcss/vite'
 import honox from 'honox/vite'
 import { defineConfig } from 'vite'
 
 export default defineConfig(({ mode }) => ({
-  server: {
-    host: '0.0.0.0',
-    port: 5173,
-    watch: {
-      usePolling: true,
-      interval: 100
-    },
-    hmr: {
-      clientPort: 5173
-    }
-  },
   plugins: [
     honox({
       devServer: { adapter },
       client: { input: ['/app/client.ts', '/app/style.css'] }
     }),
     tailwindcss(),
-    mode === 'production' && build()
   ]
 }))
