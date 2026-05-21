80b9e161c6a98a26e06637b5c15088f190507d2f
Rerurate514 <rerurateyuto@gmail.com>
2026-05-21 18:28:09 +0900

    sessionリポジトリをDI

--- app/middlewares/_di_middleware.ts
+++ app/middlewares/_di_middleware.ts
@@ -3,6 +3,7 @@ import { SqliteUsersRepositoryImpl } from "../infrastructure/repositories/sqlite
 import { SetupUsecase } from "../usecases/setup_usecase"
 import { CheckInitializeUsecase } from '../usecases/check_initialize_usecase'
 import { LoginUsecase } from "../usecases/login_usecase"
+import { SqliteSessionRepositoryImpl } from "../infrastructure/repositories/sqlite_session_repository_impl"
 
 export type DiEnv = {
     Variables: {
@@ -13,9 +14,11 @@ export type DiEnv = {
 }
 
 const userRepo = new SqliteUsersRepositoryImpl();
+const sessionRepo = new SqliteSessionRepositoryImpl();
+
 const setupUsecase = new SetupUsecase(userRepo);
 const checkInitializeUsecase = new CheckInitializeUsecase(userRepo);
-const loginUsecase = new LoginUsecase(userRepo);
+const loginUsecase = new LoginUsecase(userRepo, sessionRepo);
 
 export const diMiddleware = createMiddleware<DiEnv>(async (c, next) => {
     c.set('setupUsecase', setupUsecase);
