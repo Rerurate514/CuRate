874d1f73c0d3bba51474b48a95214454ee397f76
Rerurate514 <rerurateyuto@gmail.com>
2026-05-21 18:41:14 +0900

    loginユースケースを実装

---
+++ app/usecases/login_usecase.ts
@@ -0,0 +1,31 @@
+import { PasswordMismatchError } from "../core/exceptions/password_mismatch_error";
+import { Failure, Result, Success } from "../core/utils/result";
+import { SessionDataEntity } from "../domain/entities/session_data.entity";
+import { IDbSessionRepository } from "../domain/repositories/i_db_session_repository";
+import { IDbUsersRepository } from "../domain/repositories/i_db_users_repository";
+
+export class LoginUsecase {
+    constructor(
+        private readonly userRepo: IDbUsersRepository,
+        private readonly sessionRepo: IDbSessionRepository
+    ) {}
+
+    async execute(username: string, password: string): Promise<Result<SessionDataEntity>> {
+        const userResult = await this.userRepo.findByName(username);
+
+        if(!userResult.success) return new Failure(new UserNotFoundError());
+
+        const isValid = await Bun.password.verify(password, userResult.value!.passwordHash);
+        if(!isValid) return new Failure(new PasswordMismatchError());
+
+        const session = SessionDataEntity.create({
+            userId: userResult.value!.id,
+            expiresInSeconds: EXPIRES_IN_SECONDS
+        });
+        const result = await this.sessionRepo.create(session);
+
+        if(!result.success) return new Failure(result.error);
+        
+        return new Success(session);
+    }
+}
