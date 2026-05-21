869b3e08e50bf7b5e268545f7b11099f6596c2bc
Rerurate514 <rerurateyuto@gmail.com>
2026-05-21 19:23:18 +0900

    クッキー識別子を定義

---
+++ app/domain/constants/cookie_identifier.ts
@@ -0,0 +1,1 @@
+export const COOKIE_IDENTIFIER = "curate_session";
--- app/middlewares/_auth_middleware.ts
+++ app/middlewares/_auth_middleware.ts
@@ -1,9 +1,10 @@
 import { getCookie } from "hono/cookie";
 import { createMiddleware } from "hono/factory";
 import { BYPASS_PATHS } from "../domain/constants/bypass_paths";
+import { COOKIE_IDENTIFIER } from "../domain/constants/cookie_identifier";
 
 export const authMiddleware = createMiddleware(async (c, next) => {
-    const sessionId = getCookie(c, 'curate_session')
+    const sessionId = getCookie(c, COOKIE_IDENTIFIER)
     const { pathname } = new URL(c.req.url)
 
     if(BYPASS_PATHS.some(p => pathname.startsWith(p))) {
