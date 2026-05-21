675944b74ed195711f3465c56542002c1be17c1e
Rerurate514 <rerurateyuto@gmail.com>
2026-05-22 04:32:19 +0900

    GetDriveEntriesUsecaseをDI

--- app/middlewares/_di_middleware.ts
+++ app/middlewares/_di_middleware.ts
@@ -7,30 +7,36 @@ import { SqliteSessionRepositoryImpl } from "../infrastructure/repositories/sqli
 import { CheckValidSessionUsecase } from './../usecases/check_valid_session_usecase';
 import { LocalFileStorageRepository } from "../infrastructure/repositories/local_file_storage_repository_impl"
 import { DRIVE_DIR } from "../domain/constants/file_names"
+import { LocalDriveControllRepositoryImpl } from "../infrastructure/repositories/local_drive_controll_repository_impl"
+import { GetDriveEntriesUsecase } from "../usecases/get_drive_entries_usecase"
 
 export type DiEnv = {
     Variables: {
         setupUsecase: SetupUsecase,
         checkInitializeUsecase: CheckInitializeUsecase,
         loginUsecase: LoginUsecase,
-        checkValidSessionUsecase: CheckValidSessionUsecase
+        checkValidSessionUsecase: CheckValidSessionUsecase,
+        getDriveEntriesUsecase: GetDriveEntriesUsecase
     }
 }
 
 const userRepo = new SqliteUsersRepositoryImpl();
 const sessionRepo = new SqliteSessionRepositoryImpl();
 const fileRepo = new LocalFileStorageRepository(DRIVE_DIR);
+const driveRepo = new LocalDriveControllRepositoryImpl();
 
 const setupUsecase = new SetupUsecase(userRepo);
 const checkInitializeUsecase = new CheckInitializeUsecase(userRepo);
 const loginUsecase = new LoginUsecase(userRepo, sessionRepo);
 const checkValidSessionUsecase = new CheckValidSessionUsecase(sessionRepo);
+const getDriveEntriesUsecase = new GetDriveEntriesUsecase(driveRepo);
 
 export const diMiddleware = createMiddleware<DiEnv>(async (c, next) => {
     c.set('setupUsecase', setupUsecase);
     c.set('checkInitializeUsecase', checkInitializeUsecase);
     c.set('loginUsecase', loginUsecase);
     c.set('checkValidSessionUsecase', checkValidSessionUsecase);
+    c.set('getDriveEntriesUsecase', getDriveEntriesUsecase);
 
     await next();
 })
