c2a7cb35940b69e741dd5bbc8af6b0efe4f6ef27
Rerurate514 <rerurateyuto@gmail.com>
2026-05-21 22:55:13 +0900

    セッションチェックのユースケースを作成

--- app/middlewares/_di_middleware.ts
+++ app/middlewares/_di_middleware.ts
@@ -4,12 +4,14 @@ import { SetupUsecase } from "../usecases/setup_usecase"
 import { CheckInitializeUsecase } from '../usecases/check_initialize_usecase'
 import { LoginUsecase } from "../usecases/login_usecase"
 import { SqliteSessionRepositoryImpl } from "../infrastructure/repositories/sqlite_session_repository_impl"
+import { CheckValidSessionUsecase } from './../usecases/check_valid_session_usecase';
 
 export type DiEnv = {
     Variables: {
         setupUsecase: SetupUsecase,
         checkInitializeUsecase: CheckInitializeUsecase,
-        loginUsecase: LoginUsecase
+        loginUsecase: LoginUsecase,
+        checkValidSessionUsecase: CheckValidSessionUsecase
     }
 }
 
@@ -19,11 +21,13 @@ const sessionRepo = new SqliteSessionRepositoryImpl();
 const setupUsecase = new SetupUsecase(userRepo);
 const checkInitializeUsecase = new CheckInitializeUsecase(userRepo);
 const loginUsecase = new LoginUsecase(userRepo, sessionRepo);
+const checkValidSessionUsecase = new CheckValidSessionUsecase(sessionRepo);
 
 export const diMiddleware = createMiddleware<DiEnv>(async (c, next) => {
     c.set('setupUsecase', setupUsecase);
     c.set('checkInitializeUsecase', checkInitializeUsecase);
     c.set('loginUsecase', loginUsecase);
+    c.set('checkValidSessionUsecase', checkValidSessionUsecase);
 
     await next();
 })
---
+++ app/usecases/check_valid_session_usecase.ts
@@ -0,0 +1,17 @@
+import { Result, Success } from "../core/utils/result";
+import { IDbSessionRepository } from "../domain/repositories/i_db_session_repository";
+
+export class CheckValidSessionUsecase {
+    constructor(
+        private readonly sessionRepo: IDbSessionRepository
+    ) { }
+
+    async execute(sessionId: string): Promise<Result<boolean>> {
+        const result = await this.sessionRepo.findById(sessionId);
+        if(!result.success) return result;
+        const value = result.value;
+        if(value == null) return new Success(false);
+        if(value.expiresAt.isPast()) return new Success(false);
+        return new Success(true);
+    }
+}
