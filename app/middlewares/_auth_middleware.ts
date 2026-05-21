ba54ec3f67236aede594cebe91ed5309a169ca17
Rerurate514 <rerurateyuto@gmail.com>
2026-05-21 23:42:43 +0900

    認証ミドルウェアを作成

--- app/middlewares/_auth_middleware.ts
+++ app/middlewares/_auth_middleware.ts
@@ -2,8 +2,9 @@ import { getCookie } from "hono/cookie";
 import { createMiddleware } from "hono/factory";
 import { BYPASS_PATHS } from "../domain/constants/bypass_paths";
 import { COOKIE_IDENTIFIER } from "../domain/constants/cookie_identifier";
+import { DiEnv } from "./_di_middleware";
 
-export const authMiddleware = createMiddleware(async (c, next) => {
+export const authMiddleware = createMiddleware<DiEnv>(async (c, next) => {
     const sessionId = getCookie(c, COOKIE_IDENTIFIER)
     const { pathname } = new URL(c.req.url)
 
@@ -12,13 +13,16 @@ export const authMiddleware = createMiddleware(async (c, next) => {
         return
     }
 
-    if(!sessionId || !(await isValidSsession(sessionId))) {
+    if(!sessionId) {
+        return c.redirect('/auth/login')
+    }
+
+    const usecase = c.get('checkValidSessionUsecase');
+    const result = await usecase.execute(sessionId);
+
+    if(!result.success || !result.value) {
         return c.redirect('/auth/login')
     }
 
     await next()
 })
-
-async function isValidSsession(id: string): Promise<boolean> {
-    return true
-}
