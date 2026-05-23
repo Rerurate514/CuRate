f60eab654ef5f82a5df1c96af84280f4a36c5057
Rerurate514 <rerurateyuto@gmail.com>
2026-05-24 02:07:58 +0900

    ディレクトリを作成するユースケースを作成

---
+++ app/usecases/create_directory_usecase.ts
@@ -0,0 +1,18 @@
+import { DirectoryAlreadyExistsError } from "../core/exceptions/directory_already_exists_error";
+import { Failure, Result } from "../core/utils/result";
+import { ILocalDirectoryRepository } from "../domain/repositories/i_local_directory_repository";
+
+export class CreateDirectoryUsecase {
+    constructor(
+        private readonly dirRepo: ILocalDirectoryRepository
+    ) {}
+
+    async execute(dirPath: string): Promise<Result<void>> {
+        const existResult = await this.dirRepo.exists(dirPath);
+        if(!existResult.success) return new Failure(existResult.error);
+        if(existResult.value) return new Failure(new DirectoryAlreadyExistsError());
+
+        const result = await this.dirRepo.create(dirPath);
+        return result;
+    }
+}
