71ceeef19649c6a55321aceecea52e0baaebf493
Rerurate514 <rerurateyuto@gmail.com>
2026-05-25 13:39:23 +0900

    ファイルアップロードのusecaseを追加

---
+++ app/usecases/upload_files_usecase.ts
@@ -0,0 +1,33 @@
+import { NoFilesProvidedError } from "../core/exceptions/no_files_provided_error";
+import { Failure, Result, Success } from "../core/utils/result";
+import { IFileStorageRepository } from "../domain/repositories/i_file_storage_repository";
+import path from "node:path";
+
+export class UploadFilesUsecase {
+  constructor(
+    private readonly fileRepo: IFileStorageRepository
+  ) {}
+
+  async execute(dirPath: string, files: File[]): Promise<Result<void>> {
+    try {
+      if (files.length === 0) {
+        return new Failure(new NoFilesProvidedError());
+      }
+
+      for (const file of files) {
+        const fileFullPath = path.join(dirPath, file.name);
+        
+        const arrayBuffer = await file.arrayBuffer();
+        const buffer = Buffer.from(arrayBuffer);
+
+        const result = await this.fileRepo.save(fileFullPath, buffer);
+        
+        if (!result.success) return result;
+      }
+
+      return new Success();
+    } catch (e: any) {
+      return new Failure(e);
+    }
+  }
+}
