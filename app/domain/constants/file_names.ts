9d2a22180de0f547ba32515753cbecd77f21915f
Rerurate514 <rerurateyuto@gmail.com>
2026-05-25 15:28:55 +0900

    driveパス配下になるように修正

--- app/domain/constants/file_names.ts
+++ app/domain/constants/file_names.ts
@@ -1,3 +1,5 @@
+import * as path from "node:path";
+
 export const DB_FILE_NAME = "curate.db";
 export const BASE_DRIVE_DIR = "drive/";
-export const DRIVE_DIR = process.env.DRIVE_PATH!;
+export const DRIVE_DIR = path.join(process.env.DRIVE_PATH!, BASE_DRIVE_DIR);
