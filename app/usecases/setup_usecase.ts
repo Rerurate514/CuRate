51bbbc19f1fd378a24f96e9d35949871309dcaf9
Rerurate514 <rerurateyuto@gmail.com>
2026-05-25 14:49:12 +0900

    envusecaseをsetupに統合

--- app/usecases/setup_usecase.ts
+++ app/usecases/setup_usecase.ts
@@ -1,9 +1,13 @@
-import { Result } from "../core/utils/result";
+import { Failure, Result, Success } from "../core/utils/result";
 import { UserEntity } from "../domain/entities/user.entity";
 import { IDbUsersRepository } from "../domain/repositories/i_db_users_repository";
+import { IEnvConfigRepository } from "../domain/repositories/i_env_config_repository";
 
 export class SetupUsecase {
-  constructor(private readonly userRepo: IDbUsersRepository) {}
+  constructor(
+    private readonly userRepo: IDbUsersRepository,
+    private readonly envRepo: IEnvConfigRepository
+  ) {}
 
   async execute(username: string, password: string): Promise<Result<void>> {
     const user = await UserEntity.create({
@@ -12,6 +16,16 @@ export class SetupUsecase {
       role: "admin",
     });
 
-    return await this.userRepo.add(user);
+    const userCreateResult = await this.userRepo.add(user);
+    if(!userCreateResult.success) {
+      return new Failure(userCreateResult.error);
+    }
+
+    const resolveEnvResult = this.envRepo.resolveDrivePath();
+    if(!resolveEnvResult.success) {
+      return new Failure(resolveEnvResult.error);
+    }
+
+    return new Success();
   }
 }
