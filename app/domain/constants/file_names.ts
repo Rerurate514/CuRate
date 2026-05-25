4fa66ce6a070c650228e1235423bf78cf7830d47
Rerurate514 <rerurateyuto@gmail.com>
2026-05-25 13:59:59 +0900

    BASE_DRIVE_DIRパスを追加

--- app/domain/constants/file_names.ts
+++ app/domain/constants/file_names.ts
@@ -1,4 +1,5 @@
 import * as path from "node:path";
 
 export const DB_FILE_NAME = "curate.db";
-export const DRIVE_DIR = path.resolve(process.cwd(), "drive/");
+export const BASE_DRIVE_DIR = "drive/";
+export const DRIVE_DIR = path.resolve(process.cwd(), BASE_DRIVE_DIR);
