910c6265129b9230d33a18f041be37d3da904853
Rerurate514 <rerurateyuto@gmail.com>
2026-05-25 12:01:09 +0900

    driveファイルが生成されていなかった際の処理を追加

--- app/usecases/get_drive_entries_usecase.ts
+++ app/usecases/get_drive_entries_usecase.ts
@@ -1,13 +1,20 @@
-import { Result } from "../core/utils/result";
+import { Failure, Result } from "../core/utils/result";
 import { IDriveControllRepository } from "../domain/repositories/i_drive_controll_repository";
 import { TargetEntries } from "../domain/vo/target_entries";
 
 export class GetDriveEntriesUsecase {
     constructor(
-        private readonly driveRepo: IDriveControllRepository
+        private readonly driveRepo: IDriveControllRepository,
     ) { }
     
     async execute(targetPath: string): Promise<Result<TargetEntries>> {
+        const existResult = await this.driveRepo.existsDriveDir();
+        if(!existResult.success) return new Failure(existResult.error);
+        if(!existResult.value) {
+            const createResult = await this.driveRepo.createDriveDir();
+            if(!createResult.success) return new Failure(createResult.error);
+        }
+
         return await this.driveRepo.getTargetEntries(targetPath);
     }
 }
