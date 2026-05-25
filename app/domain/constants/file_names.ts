1a218f8ef806dad6092564580748a7939d7451ec
Rerurate514 <rerurateyuto@gmail.com>
2026-05-25 14:26:36 +0900

    外部を参照できるように変更

--- app/domain/constants/file_names.ts
+++ app/domain/constants/file_names.ts
@@ -2,4 +2,4 @@ import * as path from "node:path";
 
 export const DB_FILE_NAME = "curate.db";
 export const BASE_DRIVE_DIR = "drive/";
-export const DRIVE_DIR = path.resolve(process.cwd(), BASE_DRIVE_DIR);
+export const DRIVE_DIR = path.resolve(process.env.DRIVE_PATH ?? process.cwd(), BASE_DRIVE_DIR);
