6a2997e40917031b2f699ba27bec2dd3efeb1920
Rerurate514 <rerurateyuto@gmail.com>
2026-05-25 22:27:26 +0900

    deleteファイルユースケースを作成

---
+++ app/usecases/delete_file_usecase.ts
@@ -0,0 +1,15 @@
+import { Failure, Result, Success } from "../core/utils/result";
+import { IFileStorageRepository } from "../domain/repositories/i_file_storage_repository";
+
+export class DeleteFileUsecase {
+    constructor(
+        private readonly fileRepo: IFileStorageRepository
+    ) { }
+
+    async execute(targetPath: string): Promise<Result<void>> {
+        const deleteResult = await this.fileRepo.delete(targetPath);
+        if(!deleteResult.success) return new Failure(deleteResult.error);
+
+        return new Success();
+    }
+}
