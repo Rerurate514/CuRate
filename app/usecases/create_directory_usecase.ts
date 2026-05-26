ceafdf71f565fe5d200f37095e60d496475dcf2b
Rerurate514 <rerurateyuto@gmail.com>
2026-05-26 13:46:51 +0900

    fullパスをusecase側で組み立てないように修正

--- app/usecases/create_directory_usecase.ts
+++ app/usecases/create_directory_usecase.ts
@@ -8,13 +8,12 @@ export class CreateDirectoryUsecase {
   constructor(private readonly dirRepo: ILocalDirectoryRepository) {}
 
   async execute(dirPath: string): Promise<Result<void>> {
-    const fullPath = join(DRIVE_DIR, dirPath);
-    const existResult = await this.dirRepo.exists(fullPath);
+    const existResult = await this.dirRepo.exists(dirPath);
     if (!existResult.success) return new Failure(existResult.error);
     if (existResult.value)
       return new Failure(new DirectoryAlreadyExistsError());
 
-    const result = await this.dirRepo.create(fullPath);
+    const result = await this.dirRepo.create(dirPath);
     return result;
   }
 }
