59cc86f8527c54c731d46c6d4965c84bb4a17a5d
Rerurate514 <rerurateyuto@gmail.com>
2026-05-27 16:42:20 +0900

    donwloadContentsUsecaseファイルの仮作成

---
+++ app/usecases/donwload_contents_usecase.ts
@@ -0,0 +1,14 @@
+// import { IFileStorageRepository } from "../domain/repositories/i_file_storage_repository";
+// import { IDirectoryStorageRepository } from "../domain/repositories/i_directory_storage_repository";
+// import { Result } from "../core/utils/result";
+
+// export class DownloadContentsUsecase {
+//     constructor(
+//         private readonly fileRepo: IFileStorageRepository,
+//         private readonly dirRepo: IDirectoryStorageRepository
+//     ) {}
+
+//     async execute(): Promise<Result<void>> {
+        
+//     }
+// }
