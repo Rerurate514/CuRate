dd9ff970456ec60b050708512b5134fd9685c7af
Rerurate514 <rerurateyuto@gmail.com>
2026-05-28 15:43:28 +0900

    パンくずリストをheaderに組み込み

--- app/presentation/drive/header.tsx
+++ app/presentation/drive/header.tsx
@@ -1,4 +1,5 @@
 import { FilePicker } from "../../islands/fpicker";
+import { BreadCrumb } from "./breadcrumb";
 import { CreateFolderButton } from "./create_folder_button";
 
 type Props = {
@@ -7,10 +8,15 @@ type Props = {
 
 export default function DriveHeader({ path }: Props) {
   return (
-    <div className="bg-white p-3 border-b border-gray-200 w-full">
-      <div className="mx-auto flex items-center gap-3 w-full [&>honox-island]:flex-1">
-        <CreateFolderButton currentPath={path} />
-        <FilePicker path={path} />
+    <div>
+      <div class="mr-4 ml-4 mt-2">
+        <BreadCrumb path={path}/>
+      </div>
+      <div className="bg-white p-3 border-b border-gray-200 w-full">
+        <div className="mx-auto flex items-center gap-3 w-full [&>honox-island]:flex-1">
+          <CreateFolderButton currentPath={path} />
+          <FilePicker path={path} />
+        </div>
       </div>
     </div>
   );
