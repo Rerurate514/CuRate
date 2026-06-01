7308019d64611b68290c122aca59e658796ef560
Rerurate514 <rerurateyuto@gmail.com>
2026-06-01 12:59:54 +0900

    deleteFileスキーマをContentsとしてdirと統合

---
+++ app/domain/schemas/delete_contents.schema.ts
@@ -0,0 +1,6 @@
+import z from "zod";
+
+export const deleteContentsSchema = z.object({
+  targetPath: z.string().min(1),
+  contentsType: z.enum(['file', 'directory'])
+});
--- app/domain/schemas/delete_file.schema.ts
+++
@@ -1,5 +0,0 @@
-import z from "zod";
-
-export const deleteFileSchema = z.object({
-  targetPath: z.string().min(1),
-});
