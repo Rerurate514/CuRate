6765ecfd6f6d25755a04e7220df781179dcd1007
Rerurate514 <rerurateyuto@gmail.com>
2026-06-01 16:11:20 +0900

    format

--- app/routes/api/drive/download-dir.ts
+++ app/routes/api/drive/download-dir.ts
@@ -17,7 +17,7 @@ export const GET = createRoute(diMiddleware, async (c) => {
 
   const stream = result.value!;
   const fileName = path.split("\\").slice(2).join("_") || "download";
-  
+
   return new Response(stream, {
     headers: {
       "Content-Type": "application/zip",
