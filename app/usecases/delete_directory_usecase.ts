19bb2c2d7b282425f2926bbb0ebe140f66c7492b
Rerurate514 <rerurateyuto@gmail.com>
2026-06-01 12:38:07 +0900

    ディレクトリ削除のユースケースを作成

---
+++ app/usecases/delete_directory_usecase.ts
@@ -0,0 +1,14 @@
+import { Failure, Result, Success } from "../core/utils/result";
+import { IDirectoryStorageRepository } from "../domain/repositories/i_directory_storage_repository";
+
+export class DeleteDirectoryUsecase {
+    constructor(
+        private readonly dirRepo: IDirectoryStorageRepository
+    ) { }
+
+    async execute(targetPath: string): Promise<Result<void>> {
+        const result = await this.dirRepo.delete(targetPath);
+        if(!result.success) return new Failure(result.error);
+        return new Success();
+    }
+}
