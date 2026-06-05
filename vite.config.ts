7c4c1d731bd5a349b038ee38917452c24707890d
Rerurate514 <rerurateyuto@gmail.com>
2026-06-05 22:07:11 +0900

    SSR設定を適用

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,32 @@ import tailwindcss from '@tailwindcss/vite'
 import honox from 'honox/vite'
 import { defineConfig } from 'vite'
 
-export default defineConfig(({ mode }) => ({
-  plugins: [
-    honox({
-      devServer: { adapter },
-      client: { input: ['/app/client.ts', '/app/style.css'] }
-    }),
-    tailwindcss(),
-  ]
-}))
+export default defineConfig(({ mode }) => {
+  if (mode === 'client') {
+    return {
+      plugins: [
+        honox({
+          devServer: { adapter },
+          client: { input: ['/app/client.ts', '/app/style.css'] }
+        }),
+        tailwindcss(),
+      ]
+    }
+  }
+
+  return {
+    build: {
+      ssr: true,
+      rolldownOptions: {
+        input: ['./app/server.ts'],
+        external: ['bun', 'fflate'],
+      }
+    },
+    plugins: [
+      honox({
+        devServer: { adapter },
+      }),
+      tailwindcss(),
+    ]
+  }
+})
