6eba8e8b08d4fa29eacc6d291e5f0498dfa5aaaf
Rerurate514 <rerurateyuto@gmail.com>
2026-06-05 22:49:19 +0900

    CuRateディレクトリを追加

--- app/domain/constants/file_names.ts
+++ app/domain/constants/file_names.ts
@@ -1,6 +1,9 @@
 import * as path from "node:path";
 
+export const BASE_CURATE_NAME = "CuRate";
+export const BASE_CURATE_DIR = `${BASE_CURATE_NAME}/`;
+
 export const DB_FILE_NAME = "curate.db";
 export const BASE_DRIVE_NAME = "drive";
 export const BASE_DRIVE_DIR = `${BASE_DRIVE_NAME}/`;
-export const DRIVE_DIR = path.join(process.env.DRIVE_PATH!, BASE_DRIVE_DIR);
+export const DRIVE_DIR = path.join(process.env.DRIVE_PATH!, BASE_CURATE_DIR, BASE_DRIVE_DIR);
