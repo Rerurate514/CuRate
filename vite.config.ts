eec4c5b58579c7307aac28c2a304d33d20da190c
Rerurate514 <rerurateyuto@gmail.com>
2026-06-05 22:19:47 +0900

    staticを配信するように修正

--- vite.config.ts
+++ vite.config.ts
@@ -19,6 +19,7 @@ export default defineConfig(({ mode }) => {
   return {
     build: {
       ssr: true,
+      emptyOutDir: false, 
       rolldownOptions: {
         input: ['./app/server.ts'],
         external: ['bun', 'fflate'],
