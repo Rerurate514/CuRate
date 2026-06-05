e722f09a7d94002f6ec75850adb82f82fce4722f
Rerurate514 <rerurateyuto@gmail.com>
2026-06-05 23:39:11 +0900

    パスを修正

--- app/routes/api/drive/create-dir.ts
+++ app/routes/api/drive/create-dir.ts
@@ -2,13 +2,16 @@ import { zValidator } from "@hono/zod-validator";
 import { createRoute } from "honox/factory";
 import { createDirSchema } from "../../../domain/schemas/create_dir.schema";
 import { diMiddleware } from "../../../middlewares/_di_middleware";
+import { DRIVE_DIR } from "../../../domain/constants/file_names";
 
 export const POST = createRoute(
   diMiddleware,
   zValidator("form", createDirSchema),
   async (c) => {
     const body = c.req.valid("form");
-    const targetPath = `${body.currentPath}/${body.folderName}`;
+    const targetPath = `${DRIVE_DIR}${body.currentPath}/${body.folderName}`
+      .replaceAll("\\drive\\", "")
+      .replaceAll("\\", "/");
 
     const usecase = c.get("createDirectoryUsecase");
     const result = await usecase.execute(targetPath);
